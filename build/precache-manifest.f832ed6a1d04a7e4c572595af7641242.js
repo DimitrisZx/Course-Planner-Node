f5a7952a39a898677f8e85e5f73b6dfde1707843
dimitris <dimitriszarahanis@gmail.com>
2021-03-17 01:58:26 +0200

    add build folder

---
+++ build/precache-manifest.f832ed6a1d04a7e4c572595af7641242.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efc75d52b8f9b742485b4463d7d0bac3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8e075c6893ca362bef7",
+    "url": "/static/css/2.1afe660f.chunk.css"
+  },
+  {
+    "revision": "bf5f8c1c867f2840103b",
+    "url": "/static/css/main.f39edea9.chunk.css"
+  },
+  {
+    "revision": "e8e075c6893ca362bef7",
+    "url": "/static/js/2.3dea1b88.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/static/js/2.3dea1b88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf5f8c1c867f2840103b",
+    "url": "/static/js/main.8e8fff36.chunk.js"
+  },
+  {
+    "revision": "11bef393a0d8b282e4f5",
+    "url": "/static/js/runtime-main.08566b60.js"
+  }
+]);
